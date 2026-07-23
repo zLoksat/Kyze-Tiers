fc03d08ef95e7db54ca8cca77a097d0995696af4
zLoksat <dominguesp652@gmail.com>
2026-07-23 20:28:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1090,4 +1090,49 @@ window.closeAdmin = closeAdmin;
 window.trocarModo = trocarModo;
 window.adicionarPlayer = adicionarPlayer;
 window.removerPlayer = removerPlayer;
-window.renomearPlayer = renomearPlayer;
+window.renomearPlayer = renomearPlayer;const canvas = document.getElementById("stars");
+const ctx = canvas.getContext("2d");
+
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+resize();
+window.addEventListener("resize", resize);
+
+const stars = [];
+
+for (let i = 0; i < 120; i++) {
+    stars.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        size: Math.random() * 2 + 0.5,
+        speed: Math.random() * 0.5 + 0.2
+    });
+}
+
+function animate() {
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "white";
+
+    for (const star of stars) {
+
+        ctx.beginPath();
+        ctx.arc(star.x, star.y, star.size, 0, Math.PI * 2);
+        ctx.fill();
+
+        star.y += star.speed;
+
+        if (star.y > canvas.height + 5) {
+            star.y = -5;
+            star.x = Math.random() * canvas.width;
+        }
+    }
+
+    requestAnimationFrame(animate);
+}
+
+animate();
